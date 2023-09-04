954150b8eea5a37ee05bf677c6b3920a0464223d
Fabian Schlager <fabian.schlager@skyr.at>
2023-09-04 16:27:22 +0200

    Initial commit

--- main.ts
+++ main.ts
@@ -1,86 +1,57 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { App, Editor, FuzzySuggestModal, MarkdownView, Plugin, PluginSettingTab, Setting, TFile } from 'obsidian';
 
 // Remember to rename these classes and interfaces!
 
-interface MyPluginSettings {
-	mySetting: string;
+interface RandomPickerPluginSettings {
+	listsFolder: string;
+	stripListSymbols: boolean;
+	insertSpace: boolean;
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
+const DEFAULT_SETTINGS: RandomPickerPluginSettings = {
+	listsFolder: 'Random/',
+	stripListSymbols: true,
+	insertSpace: true
 }
 
 export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+	settings: RandomPickerPluginSettings;
 
 	async onload() {
 		await this.loadSettings();
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
 		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
+			id: 'insert-random-pick',
+			name: 'Insert random pick',
+			editorCheckCallback: (checking: boolean, editor: Editor, _view: MarkdownView) => {
 				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
+
 				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
 					if (!checking) {
-						new SampleModal(this.app).open();
+						new RandomKindSelectorModal(this.app, this.settings.listsFolder, this.settings.stripListSymbols, (item: Promise<string>) => {
+							item.then(text => {
+								if (this.settings.insertSpace) {
+									text += ' '
+								}
+
+								editor.replaceRange(text, editor.getCursor())
+
+								// Move cursor to the end of the inserted text
+								const cursorPosition = editor.posToOffset(editor.getCursor()) + text.length
+								editor.setCursor(editor.offsetToPos(cursorPosition))
+							})
+						}).open();
 					}
 
-					// This command will only show up in Command Palette when the check function returns true
 					return true;
 				}
 			}
 		});
 
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
+		this.addSettingTab(new SettingTab(this.app, this));
 	}
 
-	onunload() {
-
-	}
+	onunload() { }
 
 	async loadSettings() {
 		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
@@ -91,24 +62,51 @@ export default class MyPlugin extends Plugin {
 	}
 }
 
-class SampleModal extends Modal {
-	constructor(app: App) {
+class RandomKindSelectorModal extends FuzzySuggestModal<TFile> {
+	callback: (item: Promise<string>) => void
+	folder: string
+	randomLists: Map<string, string[]>
+	stripListSymbols: boolean
+
+	constructor(app: App, folder: string, stripListSymbols: boolean, callback: (item: Promise<string>) => void) {
 		super(app);
+		this.callback = callback;
+		this.randomLists = new Map()
+		this.folder = folder
+		this.stripListSymbols = stripListSymbols
 	}
 
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
+	getItems(): TFile[] {
+		const files = this.app.vault.getFiles()
+			.filter(f => f.path.startsWith(this.folder))
+
+		return files
 	}
 
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
+	getItemText(item: TFile): string {
+		return item.basename
+	}
+
+	onChooseItem(item: TFile, _evt: MouseEvent | KeyboardEvent): void {
+		const chosenItem = this.app.vault.cachedRead(item).then(text => {
+			const items = text.split("\n").filter(l => l.trim().length > 0)
+			const item = items[Math.floor(Math.random() * items.length)].trim()
+
+			// Remove bullet list symbols. There's probably a better way to do this
+			if (this.stripListSymbols && (item.startsWith("* ") || item.startsWith("- "))) {
+				return item.slice(2)
+			}
+
+			return item
+		})
+
+		this.callback(chosenItem)
 	}
 }
 
-class SampleSettingTab extends PluginSettingTab {
+class SettingTab extends PluginSettingTab {
 	plugin: MyPlugin;
+	warnText: HTMLElement;
 
 	constructor(app: App, plugin: MyPlugin) {
 		super(app, plugin);
@@ -116,18 +114,58 @@ class SampleSettingTab extends PluginSettingTab {
 	}
 
 	display(): void {
-		const {containerEl} = this;
+		const { containerEl } = this;
 
 		containerEl.empty();
 
+		containerEl.createEl("h1", { text: "Random Picker Settings" })
 		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
+			.setHeading()
+			.setName('Data Folder')
+			.setDesc('The plugin will read all files in the folder and allows you to choose from which one to pick a random line.')
 			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
+				.setPlaceholder('e.g. Random/')
+				.setValue(this.plugin.settings.listsFolder)
 				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
+					// Make sure the folder name ends with a slash
+					if (value.endsWith("/")) {
+						value = value.slice(0, -1)
+					}
+					const folder = this.app.vault.getAbstractFileByPath(value)
+
+					this.warnText.setText("")
+					if (folder == null) {
+						this.warnText.setText("Folder does not exist!")
+					}
+
+					if (folder instanceof TFile) {
+						this.warnText.setText("Please specify a path to a folder!")
+					}
+
+					this.plugin.settings.listsFolder = value;
+					await this.plugin.saveSettings();
+				}));
+
+		this.warnText = containerEl.createEl("small",
+			{ text: "", cls: "random-picker-warn" })
+
+		new Setting(containerEl)
+			.setName('Strip List Symbols')
+			.setDesc('Strip Markdown list symbols (e.g. - or *) before inserting the random pick.')
+			.addToggle(toggle => toggle
+				.setValue(this.plugin.settings.stripListSymbols)
+				.onChange(async (value) => {
+					this.plugin.settings.stripListSymbols = value;
+					await this.plugin.saveSettings();
+				}));
+
+		new Setting(containerEl)
+			.setName('Insert Space')
+			.setDesc('Insert space after inserted text')
+			.addToggle(toggle => toggle
+				.setValue(this.plugin.settings.insertSpace)
+				.onChange(async (value) => {
+					this.plugin.settings.insertSpace = value;
 					await this.plugin.saveSettings();
 				}));
 	}
